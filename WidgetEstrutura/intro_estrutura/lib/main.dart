9718c7b98034b29e942eab92aba5683eea8656f7
duda correa <dudacorrea1116@gmail.com>
2026-05-21 08:23:28 -0300

    21.05

--- WidgetEstrutura/intro_estrutura/lib/main.dart
+++ WidgetEstrutura/intro_estrutura/lib/main.dart
@@ -1,58 +1,47 @@
-// tela para estudo dos widgets de exibição
-//  text, image, icon entre outros
+// criar o void main
+// responsável por rodar o elemento principal da aplicação 
 
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(
-    MaterialApp(
-      //configurações iniciais do App
-      //router => rotas de navegação
-      //home => pagina Inicial
-      home: MyApp(),
-      //themeApp => (Claro/Escuro)
-    ),
-  ); //gosto de colocar o MaterialApp no void main
+void main(){
+  //runApp => chama o elemetno com o materialAPP
+  runApp(MainApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+//criar a classe MainApp
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
 
-  //a estrutura da tela
+  //Construtor da Tela Estática
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      //elemento principal da tela
-      // appbar, drawer, bnBar, body, fabutton, snakebar
-      appBar: AppBar(title: Text("Exemplos de widget de exibição")),
-
-
-      body: SingleChildScrollView(
-        child: Padding(
-        padding: EdgeInsets.all(16),
-          child: Expanded(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Text(
-                  "Explorando o Flutter",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
-                ),
-                Image.network(
-                  "https://images.unsplash.com/photo-1773257618958-c2ec3e0d249f?",
-                  height: 600,
-                  fit: BoxFit.contain,),
-                
-                Image.asset( "assets/img/tigre.jpg",
-                  height: 250,
-                  fit: BoxFit.cover,)
-                  
-              ],
-            ),
+    //montar a estrutura do MaterialAPP
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text("Tela de Login"),),
+        body:Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children://permite mais de 1 abre colchetes 
+            [
+              //elementos de input de texto
+              Text("Email"),
+              TextField(textAlign: TextAlign.center,),
+              Text("Senha"),
+              TextField(textAlign: TextAlign.center,obscureText: true,),
+              TextButton(onPressed: (){}, child: Text("Enviar"))
+            ],
           ),
         ),
+        bottomNavigationBar: BottomNavigationBar(
+          items://permite mais de 1, abre colchetes
+          [
+            BottomNavigationBarItem(icon: Icon(Icons.arrow_back),label: "back"),
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+            BottomNavigationBarItem(icon: Icon(Icons.arrow_forward),label: "forward"),
+
+          ]
+         ),
       ),
     );
   }
--- WidgetEstrutura/intro_interacao/lib/main.dart
+++ WidgetEstrutura/intro_interacao/lib/main.dart
@@ -1,16 +1,23 @@
 import 'package:flutter/material.dart';
-import 'package:intro_interacao/pages/contato_page.dart';
-import 'package:intro_interacao/pages/form_page.dart';
-import 'package:intro_interacao/pages/home_page.dart';
+
+import 'pages/contato_page.dart';
+import 'pages/form_page.dart';
+import 'pages/home_page.dart';
 
 void main(List<String> args) {
   runApp(MaterialApp(
-    routes: {
-      "/":(context) => HomePage(),
-      "/form":(context) => FormPage(),
-      "/contato":(context) => ContatoPage(),
-    },
+    //sistema de rotas para navegação entre telas
+    //home: -> tela inicial
+    //form: -> tela de formulário
+    //contato: -> tela de contato
+    routes:
+      {
+        "/": (context) => HomePage(),
+        "/form": (context) => FormularioPage(),
+        "/contato": (context) => ContatoPage(),
+      } 
+    ,
+    initialRoute: "/", //direciona o aplicativo para a homepage ao abrir
 
-    initialRoute: "/",
   ));
 }
